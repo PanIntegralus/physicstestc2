5d8c2be28f6793e640093e14d3dde38f33e30c16
PanIntegralus <42538693+PanIntegralus@users.noreply.github.com>
2020-12-19 21:13:39 +0100

    a

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1608407916,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/box-sheet0.png",
+		"images/box-sheet1.png",
+		"images/thewall-sheet0.png",
+		"images/thefloor-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"media/box.m4a",
+		"media/box.ogg",
+		"media/active.m4a",
+		"media/active.ogg",
+		"media/unactive.m4a",
+		"media/unactive.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
